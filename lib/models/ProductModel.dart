bccff9801c209ff597135afb068d9dbfaba4a4a1
akshitnandan <akshitnandan@gmail.com>
2024-01-09 14:55:50 +0530

    Worked on home.dart Implemented UI of best selleing procuts using grid view.

---
+++ lib/models/ProductModel.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+ProdutModel produtModelFromJson(String str) => ProdutModel.fromJson(json.decode(str));
+
+String produtModelToJson(ProdutModel data) => json.encode(data.toJson());
+
+class ProdutModel {
+    int id;
+    String name;
+    String image;
+    int price;
+    String description;
+    bool status;
+    bool isFav;
+
+    ProdutModel({
+        required this.id,
+        required this.name,
+        required this.image,
+        required this.price,
+        required this.description,
+        required this.status,
+        required this.isFav,
+    });
+
+    factory ProdutModel.fromJson(Map<String, dynamic> json) => ProdutModel(
+        id: json["id"],
+        name: json["name"],
+        image: json["image"],
+        price: json["price"],
+        description: json["description"],
+        status: json["status"],
+        isFav: json["isFav"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "image": image,
+        "price": price,
+        "description": description,
+        "status": status,
+        "isFav": isFav,
+    };
+}
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/firebase_helper/firebaes_auth_helper.dart';
+import 'package:e_commerce/models/ProductModel.dart';
 import 'package:e_commerce/widgets/primary_button/primary_button.dart';
 import 'package:e_commerce/widgets/top_titles/top_titles.dart';
 import 'package:flutter/material.dart';
@@ -9,53 +10,126 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-      children: [
-        Padding(
-          padding: EdgeInsets.all(12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TopTitle(title: "E Commerce", subTitle: ""),
-              TextField(),
-            ],
+      resizeToAvoidBottomInset: false,
+        body: SingleChildScrollView(
+
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TopTitle(title: "E Commerce", subTitle: ""),
+                TextField(),
+              ],
+            ),
           ),
-        ),
-        Text(
-          "Categories",
-          style: TextStyle(fontSize: 16),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: List<Widget>.of(
-                categoriesList.map(
-                  (List<String> ls) => Column(
-                    children: [
-                      Card(
-                        margin: EdgeInsets.all(8),
-                        elevation: 8,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)
+          Text(
+            "Categories",
+            style: TextStyle(fontSize: 16),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List<Widget>.of(
+                  categoriesList.map(
+                    (List<String> ls) => Column(
+                      children: [
+                        Card(
+                          margin: EdgeInsets.all(8),
+                          elevation: 8,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          // padding: EdgeInsets.all(8),
+                          child: SizedBox(
+                            height: 100,
+                            width: 100,
+                            child: Image.network(
+                              ls[1],
+                              fit: BoxFit.fill,
+                            ),
+                          ),
                         ),
-                        // padding: EdgeInsets.all(8),
-                        child: SizedBox(
-                          height: 100,
-                          width: 100,
-                        child:Image.network(ls[1],fit: BoxFit.fill, ),
-                        )
-                      ),
-                      Text(ls[0])
-                    ],
+                        Text(ls[0])
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
           ),
-        )
-      ],
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: GridView.count(
+
+              // itemCount: bestProduct.length,
+              crossAxisCount: 2,
+              // shrinkWrap: true,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              // gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                mainAxisSpacing: 20,
+                crossAxisSpacing: 20,
+                childAspectRatio: 0.75,
+              //     crossAxisCount: 2, mainAxisSpacing: 20,crossAxisSpacing: 20),
+              children:[
+                for(ProdutModel curModel in bestProduct)
+
+                // ProdutModel curModel = bestProduct[index];
+                 Container(
+                  color: Colors.red.withOpacity(0.6),
+                  padding: EdgeInsets.all(5),
+                  child: Column(
+                    children: [
+                      Center(
+                        child: Card(
+                          margin: const EdgeInsets.all(8),
+                          elevation: 8,
+                          
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          // padding: EdgeInsets.all(8),
+                          child: SizedBox(
+                            height: 100,
+                            width: 100,
+                            child: Image.network(
+                              curModel.image,
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                      // Image.network(curModel.image),
+                      SizedBox(height: 12),
+                      Text(
+                        curModel.name,
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                        "₹ "+curModel.price.toString(),
+                        style: TextStyle(
+                            fontSize: 10),
+                      ),
+                      SizedBox(height: 4),
+                      PrimaryButton(onPressed: (){}, textData: "Buy"),
+                      
+                    
+                    ],
+                  ),
+                ),
+              ]
+            ),
+              
+            ),
+        
+        ],
+      ),
     )
 
         // children: [
@@ -79,7 +153,7 @@ class Home extends StatelessWidget {
 List<List<String>> categoriesList = [
   [
     "Electronic",
-    "https://png.pngtree.com/png-vector/20190120/ourlarge/pngtree-isometric-electronic-devices-collection-png-image_324124.jpg"
+    "https://th.bing.com/th/id/OIP.vR7KyNt_xNId0gbsSFSF5wHaE7?w=264&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7",
   ],
   [
     "Grocery",
@@ -91,7 +165,7 @@ List<List<String>> categoriesList = [
   ],
   [
     "Electronic",
-    "https://png.pngtree.com/png-vector/20190120/ourlarge/pngtree-isometric-electronic-devices-collection-png-image_324124.jpg"
+    "https://th.bing.com/th/id/OIP.vR7KyNt_xNId0gbsSFSF5wHaE7?w=264&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7",
   ],
   [
     "Grocery",
@@ -103,7 +177,7 @@ List<List<String>> categoriesList = [
   ],
   [
     "Electronic",
-    "https://png.pngtree.com/png-vector/20190120/ourlarge/pngtree-isometric-electronic-devices-collection-png-image_324124.jpg"
+    "https://th.bing.com/th/id/OIP.vR7KyNt_xNId0gbsSFSF5wHaE7?w=264&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7",
   ],
   [
     "Grocery",
@@ -113,4 +187,79 @@ List<List<String>> categoriesList = [
     "Toys",
     "https://static.vecteezy.com/system/resources/previews/000/108/285/large_2x/fun-kid-toys-vector.jpg"
   ],
+];
+
+List<ProdutModel> bestProduct = [
+  ProdutModel(
+      id: 1,
+      name: "Apple",
+      image:
+          "https://th.bing.com/th/id/R.b4bff991869ae63db90f2377eab29559?rik=D3Ijv6yrOMvQLA&riu=http%3a%2f%2fwww.freepngimg.com%2fdownload%2fapple%2f16-red-apple-png-image.png&ehk=w6kY7gZO7B4tysMq1Ga86cmzgqeS4NxswdcSuXEnMRE%3d&risl=&pid=ImgRaw&r=0",
+      price: 500,
+      description: "Apple is a very nutritius fruit.",
+      status: true,
+      isFav: true),
+  ProdutModel(
+      id: 2,
+      name: "Laptop",
+      image:
+          "https://th.bing.com/th/id/R.b1ec5bc05f529d4a6c1696b21a515450?rik=F9qc43QVqXXHlQ&riu=http%3a%2f%2fwww.pngall.com%2fwp-content%2fuploads%2f2016%2f05%2fLaptop-PNG.png&ehk=XZH0Y69sWL2EL80oXAA%2fZ0HnEWh%2b1LMRt5tfn5f20R4%3d&risl=&pid=ImgRaw&r=0",
+      price: 2500,
+      description: "This is a high performance laptop.",
+      status: true,
+      isFav: true),
+  ProdutModel(
+      id: 3,
+      name: "Apple",
+      image:
+          "https://th.bing.com/th/id/R.b4bff991869ae63db90f2377eab29559?rik=D3Ijv6yrOMvQLA&riu=http%3a%2f%2fwww.freepngimg.com%2fdownload%2fapple%2f16-red-apple-png-image.png&ehk=w6kY7gZO7B4tysMq1Ga86cmzgqeS4NxswdcSuXEnMRE%3d&risl=&pid=ImgRaw&r=0",
+      price: 500,
+      description: "Apple is a very nutritius fruit.",
+      status: true,
+      isFav: true),
+  ProdutModel(
+      id: 4,
+      name: "Laptop",
+      image:
+          "https://th.bing.com/th/id/R.b1ec5bc05f529d4a6c1696b21a515450?rik=F9qc43QVqXXHlQ&riu=http%3a%2f%2fwww.pngall.com%2fwp-content%2fuploads%2f2016%2f05%2fLaptop-PNG.png&ehk=XZH0Y69sWL2EL80oXAA%2fZ0HnEWh%2b1LMRt5tfn5f20R4%3d&risl=&pid=ImgRaw&r=0",
+      price: 2500,
+      description: "This is a high performance laptop.",
+      status: true,
+      isFav: true),
+  ProdutModel(
+      id: 5,
+      name: "Apple",
+      image:
+          "https://th.bing.com/th/id/R.b4bff991869ae63db90f2377eab29559?rik=D3Ijv6yrOMvQLA&riu=http%3a%2f%2fwww.freepngimg.com%2fdownload%2fapple%2f16-red-apple-png-image.png&ehk=w6kY7gZO7B4tysMq1Ga86cmzgqeS4NxswdcSuXEnMRE%3d&risl=&pid=ImgRaw&r=0",
+      price: 500,
+      description: "Apple is a very nutritius fruit.",
+      status: true,
+      isFav: true),
+  ProdutModel(
+      id: 6,
+      name: "Laptop",
+      image:
+          "https://th.bing.com/th/id/R.b1ec5bc05f529d4a6c1696b21a515450?rik=F9qc43QVqXXHlQ&riu=http%3a%2f%2fwww.pngall.com%2fwp-content%2fuploads%2f2016%2f05%2fLaptop-PNG.png&ehk=XZH0Y69sWL2EL80oXAA%2fZ0HnEWh%2b1LMRt5tfn5f20R4%3d&risl=&pid=ImgRaw&r=0",
+      price: 2500,
+      description: "This is a high performance laptop.",
+      status: true,
+      isFav: true),
+  ProdutModel(
+      id: 7,
+      name: "Apple",
+      image:
+          "https://th.bing.com/th/id/R.b4bff991869ae63db90f2377eab29559?rik=D3Ijv6yrOMvQLA&riu=http%3a%2f%2fwww.freepngimg.com%2fdownload%2fapple%2f16-red-apple-png-image.png&ehk=w6kY7gZO7B4tysMq1Ga86cmzgqeS4NxswdcSuXEnMRE%3d&risl=&pid=ImgRaw&r=0",
+      price: 500,
+      description: "Apple is a very nutritius fruit.",
+      status: true,
+      isFav: true),
+  ProdutModel(
+      id: 8,
+      name: "Laptop",
+      image:
+          "https://th.bing.com/th/id/R.b1ec5bc05f529d4a6c1696b21a515450?rik=F9qc43QVqXXHlQ&riu=http%3a%2f%2fwww.pngall.com%2fwp-content%2fuploads%2f2016%2f05%2fLaptop-PNG.png&ehk=XZH0Y69sWL2EL80oXAA%2fZ0HnEWh%2b1LMRt5tfn5f20R4%3d&risl=&pid=ImgRaw&r=0",
+      price: 2500,
+      description: "This is a high performance laptop.",
+      status: true,
+      isFav: true),
 ];
